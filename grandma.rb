44e5b6d3a4fa69e0d715d5db6ae666f3b4588244
Chance cline <clinechance@gmail.com>
2020-01-23 19:46:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,8 @@ def speak_to_grandma(phrase)
    "I LOVE YOU TOO PUMPKIN!"
  else phrase != phrase.upcase()
    "HUH?! SPEAK UP, SONNY!"
- else "NO, NOT SINCE 1938!"
+ else phrase == phrase.upcase()
+   "NO, NOT SINCE 1938!"
 end
 end
 
